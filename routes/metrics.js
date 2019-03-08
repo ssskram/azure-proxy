756a010b76c34c9f6aac4c3b034396b5ed6d37b1
ssskram <ssskram@ssskram.local>
2019-03-08 10:46:23 -0500

    taking minutes as query param to filter span on all metrics

--- routes/metrics.js
+++ routes/metrics.js
@@ -13,13 +13,14 @@ router.get('/requests',
     async function (req, res) {
         const valid = (checkToken(req.token))
         if (valid == true) {
-            fetch("https://management.usgovcloudapi.net/subscriptions/" + process.env.SUBSCRIPTION + "/resourceGroups/" + req.query.resourceGroup + "/providers/Microsoft.Web/sites/" + req.query.appName + "/metrics?$filter=name.value eq 'Requests'&api-version=2016-08-01", {
-                    method: 'get',
-                    headers: new Headers({
-                        'Authorization': 'Bearer ' + await refreshToken(),
-                        'Accept': 'application/json'
-                    })
+            const time = await getSpan(req.query.minutes)
+            fetch("https://management.usgovcloudapi.net/subscriptions/" + process.env.SUBSCRIPTION + "/resourceGroups/" + req.query.resourceGroup + "/providers/Microsoft.Web/sites/" + req.query.appName + "/metrics?$filter=(name.value eq 'Requests') and startTime eq '" + time.from + "' and endTime eq '" + time.to + "'&api-version=2016-08-01", {
+                method: 'get',
+                headers: new Headers({
+                    'Authorization': 'Bearer ' + await refreshToken(),
+                    'Accept': 'application/json'
                 })
+            })
                 .then(res => res.json())
                 .then(data => {
                     res.status(200).send(dt(data, metrics.metric).transform())
@@ -34,15 +35,17 @@ router.get('/fourHundo',
     async function (req, res) {
         const valid = (checkToken(req.token))
         if (valid == true) {
-            fetch("https://management.usgovcloudapi.net/subscriptions/" + process.env.SUBSCRIPTION + "/resourceGroups/" + req.query.resourceGroup + "/providers/Microsoft.Web/sites/" + req.query.appName + "/metrics?$filter=name.value eq 'Http4xx'&api-version=2016-08-01", {
-                    method: 'get',
-                    headers: new Headers({
-                        'Authorization': 'Bearer ' + await refreshToken(),
-                        'Accept': 'application/json'
-                    })
+            const time = await getSpan(req.query.minutes)
+            fetch("https://management.usgovcloudapi.net/subscriptions/" + process.env.SUBSCRIPTION + "/resourceGroups/" + req.query.resourceGroup + "/providers/Microsoft.Web/sites/" + req.query.appName + "/metrics?$filter=(name.value eq 'Http4xx') and startTime eq '" + time.from + "' and endTime eq '" + time.to + "'&api-version=2016-08-01", {
+                method: 'get',
+                headers: new Headers({
+                    'Authorization': 'Bearer ' + await refreshToken(),
+                    'Accept': 'application/json'
                 })
+            })
                 .then(res => res.json())
                 .then(data => {
+                    console.log(data)
                     res.status(200).send(dt(data, metrics.metric).transform())
                 })
                 .catch(err => res.status(500).send(err))
@@ -55,13 +58,14 @@ router.get('/fiveHundo',
     async function (req, res) {
         const valid = (checkToken(req.token))
         if (valid == true) {
-            fetch("https://management.usgovcloudapi.net/subscriptions/" + process.env.SUBSCRIPTION + "/resourceGroups/" + req.query.resourceGroup + "/providers/Microsoft.Web/sites/" + req.query.appName + "/metrics?$filter=name.value eq 'Http5xx'&api-version=2016-08-01", {
-                    method: 'get',
-                    headers: new Headers({
-                        'Authorization': 'Bearer ' + await refreshToken(),
-                        'Accept': 'application/json'
-                    })
+            const time = await getSpan(req.query.minutes)
+            fetch("https://management.usgovcloudapi.net/subscriptions/" + process.env.SUBSCRIPTION + "/resourceGroups/" + req.query.resourceGroup + "/providers/Microsoft.Web/sites/" + req.query.appName + "/metrics?$filter=(name.value eq 'Http5xx') and startTime eq '" + time.from + "' and endTime eq '" + time.to + "'&api-version=2016-08-01", {
+                method: 'get',
+                headers: new Headers({
+                    'Authorization': 'Bearer ' + await refreshToken(),
+                    'Accept': 'application/json'
                 })
+            })
                 .then(res => res.json())
                 .then(data => {
                     res.status(200).send(dt(data, metrics.metric).transform())
@@ -76,13 +80,14 @@ router.get('/cpu',
     async function (req, res) {
         const valid = (checkToken(req.token))
         if (valid == true) {
-            fetch("https://management.usgovcloudapi.net/subscriptions/" + process.env.SUBSCRIPTION + "/resourceGroups/app-services/providers/Microsoft.Web/serverfarms/" + req.query.serviceName + "/metrics?$filter=name.value eq 'CpuPercentage'&api-version=2016-09-01", {
-                    method: 'get',
-                    headers: new Headers({
-                        'Authorization': 'Bearer ' + await refreshToken(),
-                        'Accept': 'application/json'
-                    })
+            const time = await getSpan(req.query.minutes)
+            fetch("https://management.usgovcloudapi.net/subscriptions/" + process.env.SUBSCRIPTION + "/resourceGroups/app-services/providers/Microsoft.Web/serverfarms/" + req.query.serviceName + "/metrics?$filter=(name.value eq 'CpuPercentage') and startTime eq '" + time.from + "' and endTime eq '" + time.to + "'&api-version=2016-09-01", {
+                method: 'get',
+                headers: new Headers({
+                    'Authorization': 'Bearer ' + await refreshToken(),
+                    'Accept': 'application/json'
                 })
+            })
                 .then(res => res.json())
                 .then(data => {
                     res.status(200).send(dt(data, metrics.metric).transform())
@@ -97,13 +102,14 @@ router.get('/memory',
     async function (req, res) {
         const valid = (checkToken(req.token))
         if (valid == true) {
-            fetch("https://management.usgovcloudapi.net/subscriptions/" + process.env.SUBSCRIPTION + "/resourceGroups/app-services/providers/Microsoft.Web/serverfarms/" + req.query.serviceName + "/metrics?$filter=name.value eq 'MemoryPercentage'&api-version=2016-09-01", {
-                    method: 'get',
-                    headers: new Headers({
-                        'Authorization': 'Bearer ' + await refreshToken(),
-                        'Accept': 'application/json'
-                    })
+            const time = await getSpan(req.query.minutes)
+            fetch("https://management.usgovcloudapi.net/subscriptions/" + process.env.SUBSCRIPTION + "/resourceGroups/app-services/providers/Microsoft.Web/serverfarms/" + req.query.serviceName + "/metrics?$filter=(name.value eq 'MemoryPercentage') and startTime eq '" + time.from + "' and endTime eq '" + time.to + "'&api-version=2016-09-01", {
+                method: 'get',
+                headers: new Headers({
+                    'Authorization': 'Bearer ' + await refreshToken(),
+                    'Accept': 'application/json'
                 })
+            })
                 .then(res => res.json())
                 .then(data => {
                     res.status(200).send(dt(data, metrics.metric).transform())
@@ -112,5 +118,15 @@ router.get('/memory',
         } else res.status(403).end()
     }
 )
+
+const getSpan = span => {
+    const now = new Date()
+    const from = new Date()
+    from.setMinutes(from.getMinutes() - span)
+    return {
+        from: from.toISOString(),
+        to: now.toISOString()
+    }
+}
 
 module.exports = router
