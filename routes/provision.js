d058f443d8ab73f03b1909b9cb039952ab618f4e
ssskram <paul.marks@pittsburghpa.gov>
2019-04-23 20:34:15 -0400

    added vm endpoint

--- routes/provision.js
+++ routes/provision.js
@@ -188,6 +188,178 @@ router.post("/lambda", async (req, res) => {
     .catch(err => res.status(500).send(err));
 });
 
+// spin up new virtual machine
+router.post("/virtualMachine", async (req, res) => {
+  let vmSize;
+  if (req.query.size == "scrawny") {
+    vmSize = "Standard_B1ms";
+  } else if (req.query.size == "well-fed") {
+    vmSize = "Standard_B2s";
+  } else if (req.query.size == "beefcake") {
+    vmSize = "Standard_B4ms";
+  } else {
+    res
+      .status(500)
+      .send({
+        error:
+          "Query parameter size must be one of the following values: scrawny, well-fed, or beefcake.  Stop wasting my time, maggot."
+      });
+  }
+
+  const createIPAddress = async () => {
+    const call = await fetch(
+      "https://management.usgovcloudapi.net/subscriptions/" +
+        process.env.SUBSCRIPTION +
+        "/resourceGroups/virtual-machines/providers/Microsoft.Network/publicIPAddresses/" +
+        req.query.serverName +
+        "?api-version=2018-11-01",
+      {
+        method: "PUT",
+        headers: new Headers({
+          Authorization: "Bearer " + (await refreshToken()),
+          "Content-Type": "application/json"
+        }),
+        body: JSON.stringify({
+          location: "USGov Virginia",
+          properties: {
+            publicIPAllocationMethod: "Static",
+            idleTimeoutInMinutes: 10,
+            publicIPAddressVersion: "IPv4"
+          },
+          sku: {
+            name: "Standard"
+          }
+        })
+      }
+    ).catch(err => res.status(500).send(err));
+    const response = await call.json();
+    return await response.name;
+  };
+
+  const createNetworkInterface = async ipAddress => {
+    const call = await fetch(
+      "https://management.usgovcloudapi.net/subscriptions/" +
+        process.env.SUBSCRIPTION +
+        "/resourceGroups/virtual-machines/providers/Microsoft.Network/networkInterfaces/" +
+        ipAddress +
+        "?api-version=2018-11-01",
+      {
+        method: "PUT",
+        headers: new Headers({
+          Authorization: "Bearer " + (await refreshToken()),
+          "Content-Type": "application/json"
+        }),
+        body: JSON.stringify({
+          location: "USGov Virginia",
+          properties: {
+            enableAcceleratedNetworking: false,
+            ipConfigurations: [
+              {
+                name: "ipconfig1",
+                properties: {
+                  publicIPAddress: {
+                    id:
+                      "/subscriptions/07fefdba-84eb-4d6b-b398-ab8737a57f95/resourceGroups/virtual-machines/providers/Microsoft.Network/publicIPAddresses/" +
+                      ipAddress
+                  },
+                  subnet: {
+                    id:
+                      "/subscriptions/07fefdba-84eb-4d6b-b398-ab8737a57f95/resourceGroups/virtual-machines/providers/Microsoft.Network/virtualNetworks/vnet/subnets/default"
+                  }
+                }
+              }
+            ]
+          }
+        })
+      }
+    ).catch(err => res.status(500).send(err));
+    const response = await call.json();
+    // why this wierd recursive shit?
+    // because the public IP is created with a "Pending" status
+    // and if the createNetworkInterface() is called, and attempts to reference the IP while still pending,
+    // then it'll throw
+    // so...keep calling createNetworkInterface() until it doesn't throw
+    if (response.name) {
+      return;
+    } else {
+      await createNetworkInterface(req.query.serverName);
+    }
+  };
+
+  const createVM = async () => {
+    await fetch(
+      "https://management.usgovcloudapi.net/subscriptions/" +
+        process.env.SUBSCRIPTION +
+        "/resourceGroups/virtual-machines/providers/Microsoft.Compute/virtualMachines/" +
+        req.query.serverName +
+        "?api-version=2018-06-01",
+      {
+        method: "PUT",
+        headers: new Headers({
+          Authorization: "Bearer " + (await refreshToken()),
+          "Content-Type": "application/json"
+        }),
+        body: JSON.stringify({
+          location: "USGov Virginia",
+          properties: {
+            hardwareProfile: {
+              vmSize: vmSize
+            },
+            osProfile: {
+              adminPassword: process.env.VM_PASSWORD,
+              adminUsername: process.env.VM_USERNAME,
+              computerName: req.query.serverName,
+              linuxConfiguration: {
+                disablePasswordAuthentication: false
+              }
+            },
+            storageProfile: {
+              imageReference: {
+                sku: "18.04-LTS",
+                publisher: "Canonical",
+                version: "latest",
+                offer: "UbuntuServer"
+              },
+              osDisk: {
+                caching: "ReadWrite",
+                managedDisk: {
+                  storageAccountType: "Standard_LRS"
+                },
+                name: req.query.serverName,
+                createOption: "FromImage"
+              }
+            },
+            networkProfile: {
+              networkInterfaces: [
+                {
+                  id:
+                    "/subscriptions/" +
+                    process.env.SUBSCRIPTION +
+                    "/resourceGroups/virtual-machines/providers/Microsoft.Network/networkInterfaces/" +
+                    req.query.serverName,
+                  properties: {
+                    primary: true
+                  }
+                }
+              ]
+            }
+          }
+        })
+      }
+    )
+      .then(res => res.json())
+      .then(data => {
+        res.status(200).send(data);
+      })
+      .then(() => tellBaloo({ type: "VM", name: req.query.serverName }))
+      .catch(err => res.status(500).send(err));
+  };
+
+  createIPAddress()
+    .then(await createNetworkInterface)
+    .then(await createVM);
+});
+
 const tellBaloo = activity => {
   fetch("https://baloo.azurewebsites.us/azMonitor/activity", {
     method: "POST",
